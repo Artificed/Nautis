518460cd7a53764d9a3a65853a49289e70d46f13
Artificed <paultsai.id@gmail.com>
2025-10-23 09:38:52 +0700

    feat: added detail sections to workplan page

--- application/src/pages/workplan/workplan-page.tsx
+++ application/src/pages/workplan/workplan-page.tsx
@@ -2,7 +2,9 @@ import CustomCursor from "../../common/shared/custom-cursor";
 import Navbar from "../../common/components/navbar";
 import HeroSection from "./components/hero-section";
 import SemesterOneSection from "./components/semester-one-section";
+import SemesterOneDetailsSection from "./components/semester-one-details-section";
 import SemesterTwoSection from "./components/semester-two-section";
+import SemesterTwoDetailsSection from "./components/semester-two-details-section";
 import Footer from "../../common/components/footer";
 
 export default function WorkplanPage() {
@@ -19,7 +21,15 @@ export default function WorkplanPage() {
         </div>
 
         <div className="snap-section">
+          <SemesterOneDetailsSection />
+        </div>
+
+        <div className="snap-section">
           <SemesterTwoSection />
+        </div>
+
+        <div className="snap-section">
+          <SemesterTwoDetailsSection />
         </div>
 
         <div className="snap-section">
